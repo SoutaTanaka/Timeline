fdc2e2b03e4c5100e46f0237da866850c6c33a02
SoutaTanaka <tanakakesouta@gmail.com>
2017-02-08 20:33:53 +0900

    commit

--- Timeline1/AppDelegate.swift
+++ Timeline1/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import JSQMessagesViewController
 import FirebaseDatabase
 
 @UIApplicationMain
@@ -18,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:
         [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-             FIRApp.configure()
+        FIRApp.configure()
         // Override point for customization after application launch.
         return true
         
--- Timeline1/MessageViewController.swift
+++ Timeline1/MessageViewController.swift
@@ -3,20 +3,110 @@
 
 import UIKit
 import JSQMessagesViewController
+import Firebase
+import FirebaseDatabase
+class MessageViewController: JSQMessagesViewController {
+    var ref = FIRDatabase.database().reference()
 
-class MessageViewController: UIViewController, JSQMessagesViewController {
-
+   
+    var messages: [JSQMessage]?
+    var incomingBubble: JSQMessagesBubbleImage!
+    var outgoingBubble: JSQMessagesBubbleImage!
+    var incomingAvatar: JSQMessagesAvatarImage!
+    var outgoingAvatar: JSQMessagesAvatarImage!
+    
+    func setupFirebase() {
+        
+        // 最新25件のデータをデータベースから取得する
+        // 最新のデータ追加されるたびに最新データを取得する
+        
+        ref.queryLimited(toLast: 25).observe(.childAdded, with: { (snapshot) in
+            guard let value = snapshot.value as? [String: [String]] else {
+                return
+            }
+            let sender = value["from"] as? String
+            let name = value["name"] as? String
+            let text = value["text"] as? String
+            print(snapshot.value!)
+            let message = JSQMessage(senderId: sender, displayName: name, text: text)
+            self.messages?.append(message!)
+            self.finishReceivingMessage()
+        })
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-     
+        // Do any additional setup after loading the view, typically from a nib.
+        inputToolbar!.contentView!.leftBarButtonItem = nil
+        automaticallyScrollsToMostRecentMessage = true
+        
+        
+        //自分のsenderId, senderDisokayNameを設定
+        self.senderId = "user1"
+        self.senderDisplayName = "hoge"
+        
+        //吹き出しの設定
+        let bubbleFactory = JSQMessagesBubbleImageFactory()
+        self.incomingBubble = bubbleFactory?.incomingMessagesBubbleImage(with: UIColor.jsq_messageBubbleLightGray())
+        self.outgoingBubble = bubbleFactory?.outgoingMessagesBubbleImage(with: UIColor.jsq_messageBubbleBlue())
+        
+        //アバターの設定
+        self.incomingAvatar = JSQMessagesAvatarImageFactory.avatarImage(with: UIImage(named: "Swift-Logo")!, diameter: 64)
+        self.outgoingAvatar = JSQMessagesAvatarImageFactory.avatarImage(with: UIImage(named: "Swift-Logo")!, diameter: 64)
+        
+        //メッセージデータの配列を初期化
+        self.messages = []
+        setupFirebase()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //Sendボタンが押された時に呼ばれる
+    func didPressSendButton(button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: NSDate!) {
         
+        //メッセジの送信処理を完了する(画面上にメッセージが表示される)
+        self.finishReceivingMessage(animated: true)
+        
+        //firebaseにデータを送信、保存する
+        let post1 = ["from": senderId, "name": senderDisplayName, "text":text]
+        let post1Ref = ref.childByAutoId()
+        post1Ref.setValue(post1)
         
     }
     
-
+    //アイテムごとに参照するメッセージデータを返す
+    func collectionView(collectionView: JSQMessagesCollectionView!, messageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageData! {
+        return self.messages?[indexPath.item]
+    }
+    
+    //アイテムごとのMessageBubble(背景)を返す
+    func collectionView(collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageBubbleImageDataSource! {
+        let message = self.messages?[indexPath.item]
+        if message?.senderId == self.senderId {
+            return self.outgoingBubble
+        }
+        return self.incomingBubble
+    }
+    
+    //アイテムごとにアバター画像を返す
+    func collectionView(collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageAvatarImageDataSource! {
+        let message = self.messages?[indexPath.item]
+        if message?.senderId == self.senderId {
+            return self.outgoingAvatar
+        }
+        return self.incomingAvatar
+    }
+    
+    //アイテムの総数を返す
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return (self.messages?.count)!
+    }
+    
+    
 }
+
